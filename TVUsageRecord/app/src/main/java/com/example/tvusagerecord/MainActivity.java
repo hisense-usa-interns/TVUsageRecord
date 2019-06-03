e843c822f3a05f037529efe479e1da8a92bb2afb
Tusheet Sidharth Goli <tgoli3@gatech.edu>
2019-06-03 10:04:52 -0400

    Add files via upload

---
+++ TVUsageRecord/app/src/main/java/com/example/tvusagerecord/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.tvusagerecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.usage.*;
+import android.content.Intent;
+import android.provider.Settings;
+import android.content.Context;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    public Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Check if permission enabled
+        if (UStats.getUsageStatsList(this).isEmpty()){
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * print the package names and duration of apps during the past one year
+     * @param context
+     */
+    public void printSortedUsageStatsList(Context context) {
+        List<UsageStats> list = UStats.getUsageStatsList(context);
+        List<UsageStats> sortedList = UStats.sortUsageStatsList(list);
+        UStats.printUsageStats(sortedList);
+    }
+
+
+
+
+
+
+
+}
---
+++ TVUsageRecord/app/src/main/java/com/example/tvusagerecord/UStats.java
@@ -0,0 +1,138 @@
+package com.example.tvusagerecord;
+
+import java.text.SimpleDateFormat;
+import android.content.Context;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import java.util.Calendar;
+import java.util.List;
+import android.util.Log;
+import java.util.Comparator;
+import java.lang.*;
+import java.util.ArrayList;
+
+public class UStats {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy HH:mm:ss");
+    public static final String TAG = UsageStats.class.getSimpleName();
+
+    /**
+     * get pkg names of usage stats within the time range based on parameters
+     * @param context
+     * @param yearNum
+     * @param monthNum
+     */
+    public void getStatsForPeriod(Context context, int yearNum, int monthNum) {
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        int interval = UsageStatsManager.INTERVAL_YEARLY;
+        //get current time as the endtime
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        //set start time based on input
+        calendar.add(Calendar.YEAR, -1 * yearNum);
+        calendar.add(Calendar.MONTH, -1 * monthNum);
+        long startTime = calendar.getTimeInMillis();
+
+        Log.d(TAG, "Range start: " + dateFormat.format(startTime));
+        Log.d(TAG, "Range end: " + dateFormat.format(endTime));
+
+        UsageEvents uEvents = usm.queryEvents(startTime, endTime);
+        while (uEvents.hasNextEvent()) {
+            UsageEvents.Event event = new UsageEvents.Event();
+            uEvents.getNextEvent(event);
+
+            if (event != null) {
+                Log.d(TAG, "Event: " + event.getPackageName() + "\t" + event.getTimeStamp());
+            }
+        }
+    }
+
+    /**
+     * Retrieve the list of usage stats in the past one year
+     * @param context
+     * @return
+     */
+    public static List<UsageStats> getUsageStatsList(Context context){
+        //UsageStatsManager usm = getUsageStatsManager(context);
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.YEAR, -1);
+        long startTime = calendar.getTimeInMillis();
+
+        Log.d(TAG, "Range start:" + dateFormat.format(startTime) );
+        Log.d(TAG, "Range end:" + dateFormat.format(endTime));
+
+        List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,startTime,endTime);
+        return usageStatsList;
+    }
+
+    /**
+     * print the given list of usage stats with its total duration in the range
+     * @param usageStatsList
+     */
+    public static void printUsageStats(List<UsageStats> usageStatsList){
+        for (UsageStats u : usageStatsList){
+            Log.d(TAG, "Pkg: " + u.getPackageName() +  "\t" + "ForegroundTime: "
+                    + u.getTotalTimeInForeground()) ;
+        }
+    }
+
+    /**
+     * Sort usage stats list given by parameter in terms of total time in foreground
+     * @param usageStatsList
+     * @return
+     */
+    public static List<UsageStats> sortUsageStatsList(List<UsageStats> usageStatsList) {
+        List<UsageStats> newList = usageStatsList;
+        newList.sort(new Comparator<UsageStats>() {
+            @Override
+            public int compare(UsageStats u1, UsageStats u2) {
+
+                if(u1.getTotalTimeInForeground() > u2.getTotalTimeInForeground()){
+                    return 1;
+                } else if (u1.getTotalTimeInForeground() < u2.getTotalTimeInForeground()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+
+            }
+        });
+        return newList;
+    }
+
+
+    /**
+     * get the sorted list of package names from given sorted UsageStats list
+     * @param sortedList
+     * @return
+     */
+    public List<String> getSortedPackageNames(List<UsageStats> sortedList) {
+        List<String> nameList = new ArrayList<String>();
+        for (int i = 0; i < sortedList.size(); i++) {
+            nameList.add(i, sortedList.get(i).getPackageName());
+        }
+        return nameList;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
