286e6ea337caed5b6e300b15ac509ec1e5e812f2
Weixiong Zhang <remus@10.1.11.109>
2019-06-07 15:11:31 -0400

    added broadcast receiver and main service to start service when the device boots up

---
+++ TVUsageRecord/app/src/main/java/com/example/tvusagerecord/MBroadcastReceiver.java
@@ -0,0 +1,38 @@
+package com.example.tvusagerecord;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.example.tvusagerecord.manager.Manager;
+
+/**
+ * receive the TV boot up broadcast to start the usage recording app and make track of
+ * apps and activities happening
+ */
+public class MBroadcastReceiver extends BroadcastReceiver {
+
+    /** unique tag for the class */
+    private static final String TAG = "MBroadcastReceiver";
+    /** manager regulating file of duration, app time stamp, etc. */
+    //Manager manager = new Manager();
+
+    /**
+     * when receive boot up intent, start the main service
+     * @param context
+     * @param intent
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e(TAG, "get action " + intent.getAction());
+        // if detected TV boot up
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent serviceIntent = new Intent(context, MainService.class);
+            context.startService(serviceIntent);
+        }
+    }
+
+
+
+}
---
+++ TVUsageRecord/app/src/main/java/com/example/tvusagerecord/MainService.java
@@ -0,0 +1,18 @@
+package com.example.tvusagerecord;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ *
+ */
+public class MainService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
