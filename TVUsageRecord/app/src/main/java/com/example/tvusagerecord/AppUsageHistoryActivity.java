3dcf9d478298e4c32c23f281838630a412c57972
Weixiong Zhang <remus@10.1.11.49>
2019-06-17 17:42:06 -0400

    added method to sort the app list in the file

--- TVUsageRecord/app/src/main/java/com/example/tvusagerecord/AppUsageHistoryActivity.java
+++ TVUsageRecord/app/src/main/java/com/example/tvusagerecord/AppUsageHistoryActivity.java
@@ -1,23 +1,35 @@
 package com.example.tvusagerecord;
 
 import android.app.usage.UsageStats;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-
+import java.io.PrintWriter;
 import com.example.tvusagerecord.io.AppTimeStampFileManager;
 import com.example.tvusagerecord.manager.Manager;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.io.FileNotFoundException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
+
 import android.util.Log;
 
 public class AppUsageHistoryActivity extends AppCompatActivity {
 
     public static final String TAG = AppUsageHistoryActivity.class.getSimpleName();
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy HH:mm:ss");
+    public static final String fileName = "app_timestamp.csv";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +58,53 @@ public class AppUsageHistoryActivity extends AppCompatActivity {
         sortFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Manager manager = new Manager();
+                List<String> fileLines = new ArrayList<>();
+                try {
+                    fileLines = manager.getAppsRunningHistory("app_timestamp.csv");
+                } catch (FileNotFoundException e) {
+                    Log.e("AppUsageHistoryActivity", "time stamp file not found");
+                }
+
+
+                fileLines.sort(new Comparator<String>() {
+                    @Override
+                    public int compare(String l1, String l2) {
+
+                        String[] l1Values = l1.split(",");
+                        String l1TimeStr = l1Values[0];
+                        String[] l2Values = l2.split(",");
+                        String l2TimeStr = l2Values[0];
+                        long time1 = 0;
+                        long time2 = 0;
+                        try {
+                            time1 = dateFormat.parse(l1TimeStr).getTime();
+                            time2 = dateFormat.parse(l2TimeStr).getTime();
+                        } catch (ParseException e) {
+                            Log.e(TAG, "parse exception when parse time stamp string to long");
+                        }
+
+                        if (time1 > time2){
+                            return 1;
+                        } else if (time1 < time2) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    }
+                });
+                try {
+                    PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+                    for (int i = 0; i < fileLines.size(); i++) {
+                        writer.println(fileLines.get(i));
+                    }
+                    writer.close();
+                } catch (FileNotFoundException e) {
+                    Log.e(TAG, "file not found when writing to file");
+                } catch (UnsupportedEncodingException e) {
+                    Log.e(TAG, "unsupported encoding when writing to file");
+                }
 
             }
         });
