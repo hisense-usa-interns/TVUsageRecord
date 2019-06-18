27a8ddc4dca51b439d009639c307e0439a240e25
Tusheet Goli <tusheetgoli@yahoo.com>
2019-06-18 18:22:41 -0400

    listView changes

--- TVUsageRecord/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ TVUsageRecord/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -78,15 +78,15 @@ public final class R {
         public static final int gone = 0x7f08004d;
         public static final int invisible = 0x7f080057;
         public static final int left = 0x7f08005c;
-        public static final int packed = 0x7f080072;
-        public static final int parent = 0x7f080074;
-        public static final int percent = 0x7f080077;
-        public static final int right = 0x7f08007d;
-        public static final int spread = 0x7f0800a1;
-        public static final int spread_inside = 0x7f0800a2;
-        public static final int start = 0x7f0800a7;
-        public static final int top = 0x7f0800bd;
-        public static final int wrap = 0x7f0800cf;
+        public static final int packed = 0x7f080073;
+        public static final int parent = 0x7f080075;
+        public static final int percent = 0x7f080078;
+        public static final int right = 0x7f08007e;
+        public static final int spread = 0x7f0800a2;
+        public static final int spread_inside = 0x7f0800a3;
+        public static final int start = 0x7f0800a8;
+        public static final int top = 0x7f0800be;
+        public static final int wrap = 0x7f0800d0;
     }
     public static final class styleable {
         private styleable() {}
--- TVUsageRecord/app/src/main/java/com/example/tvusagerecord/WeeklyUsageStats.java
+++ TVUsageRecord/app/src/main/java/com/example/tvusagerecord/WeeklyUsageStats.java
@@ -1,12 +1,23 @@
 package com.example.tvusagerecord;
 
+import android.app.usage.UsageStats;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeeklyUsageStats extends AppCompatActivity {
+
+    public static final String TAG = WeeklyUsageStats.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +46,29 @@ public class WeeklyUsageStats extends AppCompatActivity {
                 startActivity(new Intent(WeeklyUsageStats.this, WeekStats.class));
             }
         });
+
+
+        //list view - temporary code
+
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        final List<String> weekArray = new ArrayList<>();
+        weekArray.add("Week 1");
+        weekArray.add("Week 2");
+        Log.d(TAG, "weekArray "+ weekArray.get(0));
+        Log.d(TAG, "weekArray "+ weekArray.get(1));
+
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, weekArray);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(WeeklyUsageStats.this, weekArray.get(position), Toast.LENGTH_SHORT).show();
+                weekStats.setWeekNumber(position + 1);
+                startActivity(new Intent(WeeklyUsageStats.this, WeekStats.class));
+            }
+        });
+
     }
 }
