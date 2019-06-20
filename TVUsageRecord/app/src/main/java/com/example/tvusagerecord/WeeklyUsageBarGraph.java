576482357b200f1a8236b376848b7d3a8c89caad
Tusheet Goli <tusheetgoli@yahoo.com>
2019-06-20 16:41:04 -0400

    bar graph implementation

--- TVUsageRecord/app/src/main/java/com/example/tvusagerecord/WeeklyUsageBarGraph.java
+++ TVUsageRecord/app/src/main/java/com/example/tvusagerecord/WeeklyUsageBarGraph.java
@@ -2,18 +2,25 @@ package com.example.tvusagerecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.tvusagerecord.io.DurationFileManager;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class WeeklyUsageBarGraph extends AppCompatActivity {
 
+    public static final String TAG = WeeklyUsageBarGraph.class.getSimpleName();
+    public static final String durationFileName = "duration.csv";
     BarChart barChart;
 
     @Override
@@ -21,32 +28,44 @@ public class WeeklyUsageBarGraph extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_usage_bar_graph);
 
-        barChart = (BarChart) findViewById(R.id.bargraph);
+        //getting plotting data for the bar graph
+        DurationFileManager durationFileManager = new DurationFileManager();
+        final List<String> weekArray = new ArrayList<>();
+        ArrayList<Float> total = new ArrayList<>();
+
+        String[][] table = new String[0][];
+        try {
+            table = durationFileManager.readDurationFile(durationFileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for(int i = 0; i < 100; i++) {
+            float totalUsage = durationFileManager.getTotalUsage(table, (i  + 1));
+            if (totalUsage > 0) {
+                total.add(totalUsage);
+                weekArray.add("Week " + (i + 1));
+                Log.d(TAG, "weekArray: "+ (i + 1));
+            }
+        }
+
         ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(0, 44));
-        barEntries.add(new BarEntry(1, 88));
-        barEntries.add(new BarEntry(2, 66));
-        barEntries.add(new BarEntry(3, 12));
-        barEntries.add(new BarEntry(4, 19));
-        barEntries.add(new BarEntry(5, 91));
+        for (int i = 0; i < total.size(); i++) {
+            int xValue = Integer.parseInt(weekArray.get(i).split(" ")[1]);
+            float totalHours = total.get(i)/3600;
+            barEntries.add(new BarEntry(xValue - 1, totalHours));
+            Log.d(TAG, "xValue - 1: " + (xValue - 1));
+        }
+
+        barChart = (BarChart) findViewById(R.id.bargraph);
+
         BarDataSet barDataSet = new BarDataSet(barEntries, "Weeks");
-
-        ArrayList<String> weeks = new ArrayList<>();
-        weeks.add("Week 1");
-        weeks.add("Week 2");
-        weeks.add("Week 3");
-        weeks.add("Week 4");
-        weeks.add("Week 5");
-        weeks.add("Week 6");
-
         BarData theData = new BarData(barDataSet);
         barChart.setData(theData);
 
         barChart.setTouchEnabled(true);
         barChart.setDragEnabled(true);
         barChart.setScaleEnabled(true);
-        barChart.setDescription("Weekly Usage Stats");
-        barChart.setDescriptionPosition(200,1000);
         barChart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
