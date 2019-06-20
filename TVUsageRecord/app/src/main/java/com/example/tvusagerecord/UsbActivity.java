83ec5fcbbab8af6f363bd49ec2fe8b6293dcb8d1
Weixiong Zhang <remus@10.1.11.49>
2019-06-20 17:34:22 -0400

    added android manifest requirements for usb connection

---
+++ TVUsageRecord/app/src/main/java/com/example/tvusagerecord/UsbActivity.java
@@ -0,0 +1,13 @@
+package com.example.tvusagerecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsbActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usb);
+    }
+}
